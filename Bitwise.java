8569f897a88b79a15d107c5c35bb0d52bf39af6a
Sivasumithra.S <sivasumithrasivasumithra87@gmail.com>
2026-07-18 10:30:02 +0530

    Add Bitwise operations demonstration in Bitwise.java

---
+++ Bitwise.java
@@ -0,0 +1,13 @@
+public class Bitwise{
+    public static void main(String[] args) {
+        int a=5;
+        int b=3;
+        System.out.println("a&b=" + (a & b));
+        System.out.println("a|b=" + (a | b));
+        System.out.println("a^b=" + (a ^ b));
+        System.out.println("~a=" + (~a));
+        System.out.println("~b=" + (~b));
+        System.out.println("a<<2=" + (a << 2));
+        System.out.println("b>>2=" + (b >> 2));
+    }
+}
